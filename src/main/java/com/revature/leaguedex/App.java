ad1421f3861a2963286ad4ba5c28d991a9128901
Justin Freeman <justin045@revature.net>
2022-02-11 13:25:48 -0600

    New and improved leagueDex starting point.

--- src/main/java/com/revature/leaguedex/App.java
+++ src/main/java/com/revature/leaguedex/App.java
@@ -3,8 +3,6 @@ package com.revature.leaguedex;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
-import java.io.File;
-
 public class App {
 
     public static void main(String[] args) {
